4b4a809aabaffbbedb926b7199a5564a2e42e3f4
Joao <jpvieira1312@gmail.com>
2024-11-21 17:51:28 -0300

    fazendo js aba contato

---
+++ src/js/contato.js
@@ -0,0 +1,12 @@
+function telefone(){
+    alert("Nosso telefone fixo é 11-98978-6322")
+}
+function email(){
+    alert("O nosso email é Energiaverde@gmail.com")
+}
+function wpp(){
+    alert("Nosso Whatsapp é 11-91384-1530")
+}
+function insta(){
+    alert("Nosso instagram é @EnergiaVerde_br")
+}
