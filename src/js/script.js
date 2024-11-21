9831b3efafccff91e4ab401163ca0a20d91919be
Joao <jpvieira1312@gmail.com>
2024-11-21 17:54:44 -0300

    script de mudança de cores do site

---
+++ src/js/script.js
@@ -0,0 +1,7 @@
+// Adicionando uma mensagem de boas vindas
+// alert('Bem-vindo ao Site Energia Limpa!')
+
+// Função para trocar cor do body do site
+function trocar(cor){
+    document.body.style.background = cor
+}
